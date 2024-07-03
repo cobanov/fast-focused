a481c4068cf164e7861f011b2fcb2dc9f934f655
Mehmet Kahya <84154488+mehmetkahya0@users.noreply.github.com>
2024-07-04 02:03:07 +0300

    Added URL feature. Now you can read Wikipedia Articles with fast-focused

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+import re
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
+    article_url = "https://en.wikipedia.org/wiki/Alan_Turing" #Only Wikipedia article URLs
+    response = requests.get(article_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content_text = soup.find(id='mw-content-text')
+    
+    if content_text:
+        text = content_text.get_text()
+        text = re.sub(r'\[\d+\]', '', text)  # Remove reference tags like [1], [2], etc.
+    else:
+        text = "Content not found"
 
-    with open("sucveceza.txt", "r", encoding="utf8") as file:
-        text = file.read()
-
-    words = text.split()  # Split the text into words
-    return render_template("index.html", words=words)
-
+    words = text.split()
+    return render_template("index.html", words=words)  # Render the template with words
 
 if __name__ == "__main__":
     app.run(debug=True)
